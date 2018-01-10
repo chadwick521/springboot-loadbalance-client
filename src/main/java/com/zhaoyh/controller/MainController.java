bb65af4b41fdbc95dfad58553c3ab53d7c4cedce
chadwick521 <yonghui_chad@163.com>
2018-01-10 11:44:54 +0800

    MOD:remove

--- src/main/java/com/zhaoyh/controller/MainController.java
+++ src/main/java/com/zhaoyh/controller/MainController.java
@@ -1,10 +1,6 @@
 package com.zhaoyh.controller;
 
-import com.zhaoyh.LoadConfig;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,21 +11,14 @@ import org.springframework.web.client.RestTemplate;
  * @author zhaoyh
  */
 @RestController
-@RibbonClient(name = "say-hello", configuration = LoadConfig.class)
 public class MainController {
-
-    @Bean
-    @LoadBalanced
-    RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
 
     @Autowired
     private RestTemplate restTemplate;
 
     @RequestMapping(value = "/hi", method = RequestMethod.GET)
     public String say() {
-        String result = restTemplate.getForObject("http://say-hello/api/say", String.class);
+        String result = restTemplate.getForObject("http://PRODUCER/api/say", String.class);
         return result;
     }
 
